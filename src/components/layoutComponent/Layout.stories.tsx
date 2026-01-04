b46e0fb0e1584f56643441b3fb6c72bc213476f6
NikiRenegade <nikirenegade@gmail.com>
2026-01-04 10:58:11 +0700

    fix: add memoryrouter in Layout.stories.tsx

--- src/components/layoutComponent/Layout.stories.tsx
+++ src/components/layoutComponent/Layout.stories.tsx
@@ -2,15 +2,17 @@ import type { Meta, StoryObj } from '@storybook/react';
 import React from 'react';
 import { Layout } from './Layout';
 import { ThemeProvider } from '../../contexts/ThemeContext';
-
+import { MemoryRouter } from 'react-router-dom';
 const meta: Meta<typeof Layout> = {
   title: 'Components/Layout',
   component: Layout,
   decorators: [
     (Story) => (
-      <ThemeProvider>
-        <Story />
-      </ThemeProvider>
+      <MemoryRouter initialEntries={['/']}>
+        <ThemeProvider>
+          <Story />
+        </ThemeProvider>
+      </MemoryRouter>
     ),
   ],
 };
